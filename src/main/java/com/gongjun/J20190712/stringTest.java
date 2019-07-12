4aa5bc9667b6cb23a3ee64cbd3295477db0e3bd5
GongJun <28402674@qq.com>
2019-07-12 18:03:32 +0800

    new

---
+++ src/main/java/com/gongjun/J20190712/stringTest.java
@@ -0,0 +1,21 @@
+package com.gongjun.J20190712;
+
+/**
+ * @Description:
+ * @Author:GongJun
+ * @Date:2019/7/12
+ */
+public class stringTest {
+
+    /*
+    String
+    * */
+    public static void main(String[] args) {
+        //通过字面值创建
+        String str1 = "ABC";
+        String str2 = "ABC";
+
+        String str3 = new String(str1);
+
+    }
+}
