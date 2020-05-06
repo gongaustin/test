f74f89bbafe50770233e874a3aaecc9a089a3b09
GongJun <28402674@qq.com>
2020-05-06 11:22:44 +0800

    排序算法

---
+++ src/main/java/com/gongjun/J20200506/test.java
@@ -0,0 +1,31 @@
+package com.gongjun.J20200506;
+
+import org.junit.Test;
+
+
+/**
+ * @Description:
+ * @Author:GongJun
+ * @Date:2020/5/6
+ */
+public class test {
+    @Test
+    public void a(){
+
+        int[] arr = {0,4,8,9,2,89,56};
+        int temp;
+        for (int i = 0; i< arr.length;i++){
+            for (int j = 0; j < arr.length; j++) {
+                if(arr[j]>arr[i]){
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+"\t");
+        }
+    }
+
+}
