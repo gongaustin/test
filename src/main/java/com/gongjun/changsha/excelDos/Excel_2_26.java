d0ca5c2611234e1993f22eb0399e78a68863b535
GongJun <28402674@qq.com>
2020-10-16 17:00:09 +0800

    更新到2-20

--- src/main/java/com/gongjun/changsha/excelDos/Excel_2_26.java
+++ src/main/java/com/gongjun/changsha/excelDos/Excel_2_26.java
@@ -1,7 +1,7 @@
 package com.gongjun.changsha.excelDos;
 
 /**
- * @Description:处理表格922-1.xls中Sheet:"1-01",对应关系:1-01VS1-07A-cs（开放平台）
+ * @Description:处理表格922-2.xls中Sheet:"2-20",对应关系:2-20VS2-26AA（开放平台）
  * @Author: GongJun
  * @Date: Created in 10:42 2020/10/13
  */
