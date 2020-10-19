5d72c439e182ad0337e16b86d7981775fcba29a6
GongJun <28402674@qq.com>
2020-10-19 16:59:34 +0800

    科技行业:对应关系

--- src/main/java/com/gongjun/changsha/techDos/Relationship.java
+++ src/main/java/com/gongjun/changsha/techDos/Relationship.java
@@ -4,16 +4,19 @@ import org.apache.commons.io.monitor.FileEntry;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * @Description:
+ * @Description: 处理科技对应关系的类
  * @Author: GongJun
  * @Date: Created in 16:35 2020/10/19
  */
 public class Relationship {
     //对应表文件
     public static String relaFilePath = "D:\\长沙项目\\科技\\对应关系\\对应关系.txt";
-    public static void readTxtFile(){
+    public static Map<String,String> readTxtFile(){
+        Map<String,String> relaMaps = new HashMap<>();
         try {
             File file = new File(relaFilePath);
             if(file.isFile()&&file.exists()){
@@ -21,17 +24,23 @@ public class Relationship {
                 BufferedReader br = new BufferedReader(isr);
                 String line = null;
                 while ((line = br.readLine())!= null){
-                    System.out.println(line);
+                    if(line.contains(",")){
+                        String[] relas = line.split(",");
+                        if(relas.length==2) relaMaps.put(relas[0],relas[1]);
+                    }
                 }
             }
         } catch (Exception e){
             e.printStackTrace();
         }
-
+        return relaMaps;
     }
 
     @Test
     public void test(){
-        readTxtFile();
+        Map<String,String> map = readTxtFile();
+        for(String key:map.keySet()){
+            System.out.println("key= "+ key + " and value= " + map.get(key));
+        }
     }
 }
