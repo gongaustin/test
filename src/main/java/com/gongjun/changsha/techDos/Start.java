2babcee3368150e1bcbff9b899a856124b9f8266
GongJun <28402674@qq.com>
2020-10-20 11:55:54 +0800

    科技行业:常量设置

--- src/main/java/com/gongjun/changsha/techDos/Start.java
+++ src/main/java/com/gongjun/changsha/techDos/Start.java
@@ -7,6 +7,14 @@ package com.gongjun.changsha.techDos;
  */
 public class Start {
     public static void main(String[] args) {
+
+        //常量设置
+//        String dataExcelPath = "D:\\长沙项目\\科技\\原始数据\\430102.xls";
+//        String standardExcelPath = "D:\\长沙项目\\科技\\效果表\\922-4.xlsx";
+//        CommonSet.dataExcelPathSet(dataExcelPath);
+//        CommonSet.standardExcelPathSet(standardExcelPath);
+
+        //开始处理
         Excel_4_01.todo();
         Excel_4_02.todo();
         Excel_4_03.todo();
