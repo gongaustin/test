174bd9204727fe8e980bc69c85505f5a81fe05c8
GongJun <28402674@qq.com>
2020-10-26 10:49:40 +0800

    递归获取某文件夹下的所有文件

--- src/main/java/com/gongjun/changsha/tools/FileUtils.java
+++ src/main/java/com/gongjun/changsha/tools/FileUtils.java
@@ -1,6 +1,8 @@
 package com.gongjun.changsha.tools;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Description:  文件工具类
@@ -38,6 +40,35 @@ public class FileUtils {
             System.out.println("复制单个文件出错");
             e.printStackTrace();
         }
+    }
+
+    /**
+     * @param: [path, list]
+     * @description: 递归获取该目录的所有文件
+     * @author: GongJun
+     * @time: Created in 10:46 2020/10/26
+     * @modified: 
+     * @return: java.util.List<java.io.File>
+     **/
+    public static List<File> getFiles(String path, List<File> list) {
+        if(list == null) list = new ArrayList<>();
+        File file = new File(path);
+        // 如果这个路径是文件夹
+        if (file.isDirectory()) {
+            // 获取路径下的所有文件
+            File[] files = file.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                // 如果还是文件夹 递归获取里面的文件 文件夹
+                if (files[i].isDirectory()) {
+                    getFiles(files[i].getPath(),list);
+                } else {
+                    list.add(files[i]);
+                }
+            }
+        } else {
+            list.add(file);
+        }
+        return list;
     }
 
 }
