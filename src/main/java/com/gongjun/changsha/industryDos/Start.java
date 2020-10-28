d3d7200d7ec3a20f4e712eb1799c1ce9b8db455c
GongJun <28402674@qq.com>
2020-10-28 10:13:41 +0800

    工业处理代码更新

---
+++ src/main/java/com/gongjun/changsha/industryDos/Start.java
@@ -0,0 +1,9 @@
+package com.gongjun.changsha.industryDos;
+
+/**
+ * @Description:批处理类
+ * @Author: GongJun
+ * @Date: Created in 10:11 2020/10/28
+ */
+public class Start {
+}
---
+++ src/main/java/com/gongjun/changsha/industryDos/dataSumJudgement.java
@@ -0,0 +1,9 @@
+package com.gongjun.changsha.industryDos;
+
+/**
+ * @Description:判断区县数据之和是否等于全市数据(工业)
+ * @Author: GongJun
+ * @Date: Created in 10:12 2020/10/28
+ */
+public class dataSumJudgement {
+}
