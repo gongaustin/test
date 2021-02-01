3521592939ef91b3f073337a2ec8ee19c7b3ab3d
GongJun <28402674@qq.com>
2021-02-01 14:37:34 +0800

    基础练习

---
+++ src/main/java/com/gongjun/basic/excersises.java
@@ -0,0 +1,46 @@
+package com.gongjun.basic;
+
+import org.junit.Test;
+
+/**
+ * @Description:基础练习
+ * @Author: GongJun
+ * @Date: Created in 13:39 2021/2/1
+ */
+public class excersises {
+    @Test
+    public void test(){
+        //& and &&
+        int a = 1; int b = 1;
+        System.out.println(1==++a);
+        System.out.println(1==b++);
+        a = 1;  b = 1;
+        System.out.println(2==++a&a==2);
+        System.out.println(2==b++&&b==2);
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+
+        ok:
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            break ok;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            if(i>5) break;
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            if(i>5) continue;
+            System.out.println("后面的代码会执行吗？！");
+
+        }
+        System.out.println("后面的代码会执行吗？！");
+
+        //位运算
+        System.out.println(2<<3);
+    }
+}
