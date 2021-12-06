b3af5c02c4f90a66e654d610cb812acb6c45d221
GongJun <28402674@qq.com>
2021-12-06 16:04:15 +0800

    学习基础知识

---
+++ src/main/java/com/gongjun/学习MarkDown/test.java
@@ -0,0 +1,23 @@
+package com.gongjun.学习MarkDown;
+
+import org.junit.Test;
+
+/**
+ * @Description:
+ * @Author:GongJun
+ * @Date:2021/12/6
+ */
+public class test {
+
+    public final int a;
+
+    public test(){
+        a = 1;
+    }
+
+    @Test
+    public void test(){
+        System.out.println(a);
+    }
+
+}
