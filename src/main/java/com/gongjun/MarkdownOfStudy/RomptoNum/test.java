fa1a49eef74b78c59a52cfbdaf2074fe15a218b3
gongaustin <28402674@qq.com>
2022-01-06 10:15:26 +0800

    20220106

---
+++ src/main/java/com/gongjun/MarkdownOfStudy/RomptoNum/test.java
@@ -0,0 +1,9 @@
+package com.gongjun.MarkdownOfStudy.RomptoNum;
+
+public class test {
+    public static void main(String[] args) {
+
+        int a = new SolutionD().romanToInt("MCMXCIV");
+        System.out.println("a="+a);
+    }
+}
